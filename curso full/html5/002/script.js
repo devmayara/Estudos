f47268dff106ccd295ce3afbcd873e04894deb15
Mayara G. Silva <devmayara11@gmail.com>
2021-08-31 09:12:30 -0300

    add

--- curso full/html5/002/script.js
+++
@@ -1,8 +0,0 @@
-
-document.querySelector("#botao").onclick = function() {
-    var campo = document.querySelector("#campo").value
-    
-    var lista = document.querySelector("#lista")
-
-    lista.innerHTML += "<li>"+campo+"</li>"
-}
