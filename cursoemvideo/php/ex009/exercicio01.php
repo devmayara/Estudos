4afde1065343edda018d13f8fd4acf8f4ebf5627
Mayara G. Silva <devmayara11@gmail.com>
2021-09-13 15:20:18 -0300

    add

---
+++ cursoemvideo/php/ex009/exercicio01.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8"/>
+    <link rel="stylesheet" href="_css/estilo.css">
+    <title></title>
+</head>
+<body>
+    <div>
+        <?php
+            $a = isset($_GET["ano"])?$_GET["ano"]:1900;
+            $i = date ("Y") - $a;
+            echo "Você nasceu em $a e tem $i anos.<br/><br/>";
+
+            if($i >= 18) {
+                $v = "já pode votar";
+                $d = "já pode dirigir";
+            }
+            else {
+                $v = "não pode votar";
+                $d = "não pode dirigir";
+            }
+            echo "Com essa idade você $v e <br/> também $d.";
+        ?>
+    </div>
+</body>
+</html>
---
+++ cursoemvideo/php/ex009/exercicio02.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8"/>
+    <link rel="stylesheet" href="_css/estilo.css">
+    <title></title>
+</head>
+<body>
+    <div>
+        <?php
+            $a = isset($_GET["ano"])?$_GET["ano"]:1900;
+            $i = date ("Y") - $a;
+            echo "Você nasceu em $a e tem $i anos.<br/><br/>";
+
+            if ($i < 16) {
+                $tipoVoto = "não vota";
+            }
+            else {
+                if (($i >=16 && $i < 18) || ($i > 65)) {
+                    $tipoVoto = "o voto opcional!";
+                }
+                else {
+                    $tipoVoto = "o voto obrigatótio!";
+                }
+            }
+            echo "Para essa idade, $tipoVoto";
+
+
+/**
+ *  && = e
+ *  || = ou
+ */
+        ?>
+    </div>
+</body>
+</html>
