5c13efa535955c9e769cc3ea2fb81b0bc9242207
Mayara G. Silva <devmayara11@gmail.com>
2021-09-14 17:26:51 -0300

    add

--- cursoemvideo/php/ex011/03contador1.php
+++ cursoemvideo/php/ex011/03contador1.php
@@ -1,26 +1,25 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <link rel="stylesheet" href="_css/estilo.css"/>
-    <meta charset="UTF-8"/>
-    <title>Curso de PHP - CursoemVideo.com</title>
+  <link rel="stylesheet" href="_css/estilo.css"/>
+  <meta charset="UTF-8"/>
+  <title>Curso de PHP - CursoemVideo.com</title>
 </head>
 <body>
 <div>
-    <form action="03contador.php" method="GET">
-        <?php
-            Inicio :
-            <input type="number" name="i" min="0" max="10000" required>
-            <br>
-            Final :
-            <input type="number" name="i" min="0" max="10000" required>
-            <br>
-            Incremento :
-            <input type="number" name="i" min="1" max="10" required>
-        ?>
-        <br>
-        <input type="submit" value="Enviar" class="botao">
-    </form>
+<form method="GET" action="03contador2.php">
+    <?php
+      $c = $i;
+      while ($c <= $f) {
+      echo "Inicial : <input type='number' name='$i'><br>";
+      echo "Final : <input type='number' name='$f'><br>";
+      echo "Incremento : <input type='number' name='$in'><br>";
+      $c++;
+    }
+    ?>
+    <br>
+    <input type="submit" value="Enviar" class="botao">
+  </form>
 </div>
 </body>
 </html>
--- cursoemvideo/php/ex011/03contador2.php
+++ cursoemvideo/php/ex011/03contador2.php
@@ -7,9 +7,32 @@
 </head>
 <body>
 <div>
-    <?php
-        
-    ?>
+  <?php
+    $i = $i;
+    while ($i <= $f) {
+        $v = "num".$i;
+        $url = "v".$i;
+        $$v = isset($_GET[$url]) ? $_GET[$url] : 0;
+        $i++;
+    }
+    //TESTE:
+    //echo "$num1 $num2 $num3 $num4 $num5";
+
+    $i = 1;
+    while ($i <= 5) {
+        $v = "num".$i;
+        echo "Valor $i : ".$$v."<br/>";
+        $i++;
+    }
+*/
+    $c = $i;
+    while ($c <= $f) {
+      echo $c . "</br>" ;
+      $c-=$in;   //$c = $c-2; 
+
+    }
+
+  ?>
 </div>
 </body>
 </html>
