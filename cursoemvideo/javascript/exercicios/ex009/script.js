30e265d7f96d9c539bc0be57f4445f85ce0de9eb
Mayara G. Silva <devmayara11@gmail.com>
2021-12-14 21:09:03 -0300

    Update script.js

---
+++ cursoemvideo/javascript/exercicios/ex009/script.js
@@ -0,0 +1,18 @@
+/*
+var pais = 'EUA'
+console.log(`Vivendo em ${pais}`)
+if (pais == 'Brasil'){
+  console.log(`Você é Brasileiro`)
+} else {
+  console.log(`Você é Estrangeiro`)
+}
+*/
+
+
+var pais = 'EUA'
+console.log(`Vivendo em ${pais}`)
+if (pais != 'Brasil'){
+  console.log(`Você é Estrangeiro`)
+} else {
+  console.log(`Você é Brasileiro`)
+}
