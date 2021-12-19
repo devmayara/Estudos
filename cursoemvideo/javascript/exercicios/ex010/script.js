32c4230722a2066fe7b62fc763ee2992203bade6
Mayara G. Silva <devmayara11@gmail.com>
2021-12-18 21:17:31 -0300

    update

--- cursoemvideo/javascript/exercicios/ex010/script.js
+++ cursoemvideo/javascript/exercicios/ex010/script.js
@@ -3,11 +3,11 @@ function calcular() {
   var txtv = document.querySelector('input#txtvel')
   var res = document.querySelector('div#res')
   var vel = Number(txtv.value)
-  res.innerHTML = `<p>Sua velocidade atual é de <strong>${vel}</strong>Km/h</p>`
+  res.innerHTML = `<p>Sua velocidade atual é de <strong>${vel}</strong>Km/h.</p>`
   if (vel > 60) {
     res.innerHTML += `<p>Você está <strong>MULTADO!</strong> Por ecesso de velocidade.`
   }
-  res.innerHTML += `<p>Dirija sempre com cinto de segurança</p>`
+  res.innerHTML += `<p>Dirija sempre com cinto de segurança!</p>`
 
 
 }
