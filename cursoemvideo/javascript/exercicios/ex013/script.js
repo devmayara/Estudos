a8aeaff0d4b30bebd7e6302c904b841a3fd1ae5f
Mayara G. Silva <devmayara11@gmail.com>
2021-12-22 20:34:58 -0300

    update

---
+++ cursoemvideo/javascript/exercicios/ex013/script.js
@@ -0,0 +1,15 @@
+var agora = new Date()
+var diaSem = agora.getDay()
+
+/*
+    0- Domingo
+    1- Segunda
+    2- Terça
+    3- Quarta
+    4- Quinta
+    5- Sexta
+    6- Sabado
+*/
+
+console.log(diaSem)
+switch(diaSem)
