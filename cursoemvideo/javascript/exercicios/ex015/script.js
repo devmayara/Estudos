721a0c7722d4c56400b2a119325e0fe0ed844285
Mayara G. Silva <devmayara11@gmail.com>
2022-01-01 20:15:39 -0300

    update

--- cursoemvideo/javascript/exercicios/ex015/script.js
+++ cursoemvideo/javascript/exercicios/ex015/script.js
@@ -9,10 +9,31 @@ function verificar() {
     var fsex = document.getElementsByName('radsex')
     var idade = ano - Number(fano.value)
     //res.innerHTML = `Idade calculada: ${idade}`   // TESTE
+    var gênero = ''
+    var img = document.createElement('img')
+    img.setAttribute('id', 'foto')
     if (fsex[0].checked) {
       gênero = 'Homem'
+      if (idade >= 0 && idade < 10) {
+        //  CRIANÇA
+      } else if (idade < 21) {
+        //  ADOLECENTE
+      } else if (idade < 50) {
+        // ADULTO
+      } else {
+        // IDOSO
+      }
     } else if (fsex[1].checked) {
-       gênero = 'Mulher'
+        gênero = 'Mulher'
+        if (idade >= 0 && idade < 10) {
+          //  CRIANÇA
+        } else if (idade < 21) {
+          //  ADOLECENTE
+        } else if (idade < 50) {
+          //  ADULTO
+        } else {
+          //  IDOSO
+        }
     }
     res.innerHTML = `Detectamos ${gênero} com ${idade} anos.`
   }
