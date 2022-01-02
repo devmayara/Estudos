587059d3b8d4e1b093cd0738ac908b475e20ef87
Mayara G. Silva <devmayara11@gmail.com>
2022-01-02 17:38:52 -0300

    criado ambiente

--- cursoemvideo/javascript/exercicios/ex016/aula/modulo.js
+++ cursoemvideo/javascript/exercicios/ex016/aula/modulo.js
@@ -3,7 +3,7 @@ do {
   console.log(`Passo ${c}`)
   c++
 } while (c <= 6)
-
+//--------------------------------
 /*
 console.log('Tudo bem?')
 console.log('Tudo bem?')
@@ -12,7 +12,7 @@ console.log('Tudo bem?')
 console.log('Tudo bem?')
 console.log('Tudo bem?')
 */
-
+//--------------------------------
 /*
 var c = 1
 while (c <= 6) {
@@ -20,3 +20,4 @@ while (c <= 6) {
   c++
 }
 */
+//--------------------------------
