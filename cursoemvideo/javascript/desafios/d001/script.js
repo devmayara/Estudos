5b2a802b79f8613ae55e4e14142418ff7dbb20fb
Mayara G. Silva <devmayara11@gmail.com>
2022-01-13 20:20:54 -0300

    update

--- cursoemvideo/javascript/desafios/d001/script.js
+++ cursoemvideo/javascript/desafios/d001/script.js
@@ -1,18 +1,12 @@
-function tabuada() {
-  let num = document.getElementById('txtn')
-  let tab = document.getElementById('seltab')
-  if (num.value.length == 0) {
-    alert('[ERRO] Por favor, digite um número!')
-  } else {
-    let n = Number(num.value)
-    let c = 1
-    tab.innerHTML = ''
-    while (c <= 10) {
-      let item = document.createElement('option')
-      item.text = `${n} * ${c} = ${n*c}`
-      item.value = `tab${c}`
-      tab.appendChild(item)
-      c++
-    }
-  } 
+
+function button01() {
+  alert('Você clicou no 1º botão!')
+}
+
+function button02() {
+  alert('Você clicou no 2º botão!')
+}
+
+function button03() {
+  alert('Você clicou no 3º botão!')
 }
