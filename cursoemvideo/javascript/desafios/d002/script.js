6175605eba3d8be95bbab4003cf0879d76b5beae
Mayara G. Silva <devmayara11@gmail.com>
2022-01-13 20:55:09 -0300

    update

---
+++ cursoemvideo/javascript/desafios/d002/script.js
@@ -0,0 +1,6 @@
+
+function button() {
+  var nome = prompt('Qual é o seu nome?');
+  var idade = prompt(`Olá, ${nome}! Quantos anos você tem?`);
+  alert(`Acabei de conhecer ${nome}, que tem ${idade} anos de idade!`);
+}
