f5a6d8a033d34c44aff96264d82f07434e1bf506
Mayara G. Silva <devmayara11@gmail.com>
2022-01-13 20:22:57 -0300

    criei desafio d002

---
+++ cursoemvideo/javascript/desafios/d002/script.js
@@ -0,0 +1,12 @@
+
+function button01() {
+  alert('Você clicou no 1º botão!')
+}
+
+function button02() {
+  alert('Você clicou no 2º botão!')
+}
+
+function button03() {
+  alert('Você clicou no 3º botão!')
+}
