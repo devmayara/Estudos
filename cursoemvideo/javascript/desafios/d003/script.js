adc7d53efc387ac7fa7a9a5bf53ccf1a81ea3403
Mayara G. Silva <devmayara11@gmail.com>
2022-01-13 21:17:30 -0300

    update

---
+++ cursoemvideo/javascript/desafios/d003/script.js
@@ -0,0 +1,6 @@
+
+function button() {
+  var n = Number(window.prompt('Digite um número inteiro qualquer:'));
+  alert(`Antes do ${n}, temos o número ${n - 1}.
+  Depois do ${n}, temos o número ${n + 1}.`);
+}
