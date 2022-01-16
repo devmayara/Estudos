4440351b62ba8f92b49f1607148f6942d0451837
Mayara G. Silva <devmayara11@gmail.com>
2022-01-16 10:34:40 -0300

    update

--- cursoemvideo/javascript/desafios/d004/script.js
+++ cursoemvideo/javascript/desafios/d004/script.js
@@ -1,7 +1,15 @@
 
 function button() {
   var produto = prompt("Que produto você está comprando?");
+
   var custa = Number(prompt(`Quanto custa ${produto} que você está comprando?`));
+  var n1 = custa.toLocaleString('pt-BR', {style: 'currency', currency: 'BRL'})
+
   var deu = Number(prompt(`Qual foi o valor que você deu para pagar ${produto}?`));
-  alert(`Você comprou ${produto} que custou ${custa}. Deu ${deu} em dinheiro e vai receber ${deu - custa} de troco. Volte Sempre!`);
+  var n2 = deu.toLocaleString('pt-BR', {style: 'currency', currency: 'BRL'}) 
+
+  var troco = deu - custa;
+  var n3 = troco.toLocaleString('pt-BR', {style: 'currency', currency: 'BRL'}) 
+
+  alert(`Você comprou ${produto} que custou ${n1}. Deu ${n2} em dinheiro e vai receber ${n3} de troco. Volte Sempre!`);
 }
