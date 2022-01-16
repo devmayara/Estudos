ab2d54bce0c6567301f68d90b64f71b9d590c3e8
Mayara G. Silva <devmayara11@gmail.com>
2022-01-16 10:54:59 -0300

    update

---
+++ cursoemvideo/javascript/desafios/d005/script.js
@@ -0,0 +1,12 @@
+
+function button() {
+  var distancia = Number(prompt("Digite uma distância em metros (m):"));
+
+  var km = distancia / 1000;
+  var hm = distancia / 100;
+  var dam = distancia / 10;
+  var dm = distancia * 10;
+  var cm = distancia * 100;
+  var mm = distancia * 1000;
+  
+}
