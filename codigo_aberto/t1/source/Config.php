542dac1a4d083191abe4968e6f314fdf523f433e
Mayara G. Silva <devmayara11@gmail.com>
2022-02-27 14:44:15 -0300

    commit

--- codigo_aberto/t1/source/Config.php
+++ codigo_aberto/t1/source/Config.php
@@ -1,1 +1,36 @@
 <?php
+
+/**
+ * SITE CONFIG
+ */
+define("SITE", [
+    "name" => "Auth em MVC com PHP",
+    "desc" => "Aprenda a construir uma aplicação MVC com PHP",
+    "domain" => "localhost.com",
+    "root" => "http://localhost/codigoaberto/t1",
+]);
+
+/**
+ * SITE MINIFY
+ */
+if($_SERVER["SERVER_NAME"] == "localhost"){
+    "require__DIR__" . "/Minify.php";
+}
+
+/**
+ * DATABASE CONNECT
+ */
+define("DATA_LAYER_CONFIG", [
+    "driver" => "mysql",
+    "host" => "localhost",
+    "port" => "3306",
+    "dbname" => "datalayer_example",
+    "username" => "root",
+    "passwd" => "",
+    "options" => [
+        PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8",
+        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+        PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_OBJ,
+        PDO::ATTR_CASE => PDO::CASE_NATURAL
+    ]
+]);
