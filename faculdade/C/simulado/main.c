fa38ca17259a31827ddcd1abb8b3c8a287d2fd16
Mayara Silva <devmayara11@gmail.com>
2022-03-31 19:49:13 -0300

    up 31/03/2022

---
+++ faculdade/C/simulado/main.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+void main(){
+
+controle=0;
+
+       for (cont=1;cont<=20;cont++)
+
+       {  scanf ("%d",&num);
+
+          if (controle==1)
+
+            menor=num;
+
+          else
+
+             if (num
+
+                menor=num;
+
+          controle++;}
+}
