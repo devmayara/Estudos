79a3074eafa6a36b85185e883f5ca5cbd3c19f49
Mayara Silva <devmayara11@gmail.com>
2022-04-01 08:15:55 -0300

    up 01/03/2022

--- faculdade/C/04-Operações/main.c
+++ faculdade/C/04-Operações/main.c
@@ -7,6 +7,41 @@ int main(int argc, char const *argv[])
     int matricula = 12345;
 
     float preco = 80.5;
+
+    //soma
+    idade = idade + 1;
+    idade++;
+    idade += 1;
+
+    //subtração
+    idade = idade - 1;
+    idade--;
+    idade -= 1;
+
+    //multiplicação
+    idade = idade * 2;
+    idade *= 2;
+
+    //divisão
+    idade = idade / 2;
+    idade /= 2;
+
+    //resto da divisão
+    idade = idade % 2;
+    idade %= 2;
+
+    //ordem de precedência
+    idade = (idade % 1) + 2 * 7 - 3 / 2;
+    // () -> % -> + -> * -> - -> / 
+
+    //comentario
+    /*
+        comentario
+    */
+    
+    printf("Idade: %d\n", idade);
+    printf("Matricula: %d\n", matricula);
+    printf("Preco: %.2f\n", preco);
     
     return 0;
 }
---
+++ faculdade/C/05-Printf/main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(int arge, char const *argv[])
+{
+    /* code */
+    char var_char = 'c';
+    int var_int = 18;
+    float var_float = 80.5F;
+
+    printf("var_char: %c\n", var_char);
+    printf("var_int: %d\n", var_int);
+    printf("var_float: %.2f\n", var_float);
+    printf("var_string: %s\n\n", "uma frase");
+
+    return 0;
+}
