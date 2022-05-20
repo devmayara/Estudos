e380351435df50c459ded8ee29333d242c55aec9
Mayara Silva <devmayara11@gmail.com>
2022-05-20 10:07:54 -0300

    projeto-qualquer

---
+++ DIO/PHP/projeto-qualquer/vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => 'daa40f7a0fb2866ab7a8c1ecb701a000084e6ff1',
+        'name' => 'mayara/projeto-qualquer',
+        'dev' => true,
+    ),
+    'versions' => array(
+        'mayara/projeto-qualquer' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => 'daa40f7a0fb2866ab7a8c1ecb701a000084e6ff1',
+            'dev_requirement' => false,
+        ),
+        'wead/digital-cep' => array(
+            'pretty_version' => '1.0.1',
+            'version' => '1.0.1.0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../wead/digital-cep',
+            'aliases' => array(),
+            'reference' => '63b5624635c0c1e0f2020f672dcfd235dd9834ff',
+            'dev_requirement' => false,
+        ),
+    ),
+);
