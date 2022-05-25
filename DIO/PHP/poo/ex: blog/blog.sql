91638afa4404a5350772d2f68c5831a8b41b9c45
Mayara Silva <devmayara11@gmail.com>
2022-05-25 12:49:57 -0300

    Delete DIO/PHP/poo/ex: blog directory

--- DIO/PHP/poo/ex: blog/blog.sql
+++
@@ -1,9 +0,0 @@
-CREATE DATABASE blog;
-
-use blog;
-CREATE TABLE post (
-    id int(11) NOT NULL AUTO_INCREMENT,
-    titulo varchar(50) DEFAULT NULL,
-    descricao varchar(100) DEFAULT NULL,
-    PRIMARY KEY (id)
-);
