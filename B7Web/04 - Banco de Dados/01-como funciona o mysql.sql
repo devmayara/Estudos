01a0f43b6c186e718cd2c388f1de5f15c1bdcf71
Mayara Silva <devmayara11@gmail.com>
2022-06-05 13:00:55 -0300

    update

---
+++ B7Web/04 - Banco de Dados/01-como funciona o mysql.sql
@@ -0,0 +1,18 @@
+- software
+- porta padrão do mysql: 3306
+
+- 127.0.0.1:3306
+
+- C: Create = criar = inserir dados
+- R: Read = ler = pegar dados existentes
+- U: Update = atualizar = modificar dados existentes
+- D: Delete = deletar = remover dados existentes
+
+CRUD = Create, Read, Update, Delete
+
+quem usa:
+
+- Facebook
+- youtube
+- Netflix
+- airbnb
---
+++ B7Web/04 - Banco de Dados/02-Opções de informações na coluna.sql
@@ -0,0 +1,14 @@
+- cada coluna tem algumas informações SOBRE ELA:
+
+nome da coluna
+tipo da coluna
+tamanho da coluna
+se a coluna aceita valores nulos (NULL)
+valor padrão
+
+- mais comuns:
+        tinyint/int
+        float
+        varchar/text
+        date/time/datetime/timestamp
+        
