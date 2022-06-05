065402e733e8906941f30fce9d70c36a8c0e9978
Mayara Silva <devmayara11@gmail.com>
2022-06-05 16:12:12 -0300

    modulo 01

---
+++ B7Web/02 - JavaScript/m01/04-Formas de mostrar (Output)/script.js
@@ -0,0 +1,7 @@
+document.getElementById('titulo').innerHTML = "Opa, tudo bem?"; // Altera o texto do elemento com id titulo
+
+document.write("<h1>Olá mundo!</h1>"); // Escreve no console  // quase nunca usado
+
+alert("Olá mundo!"); // Mostra uma caixa de mensagem
+
+console.log("Olá Console!"); // Mostra uma caixa de mensagem no console
---
+++ B7Web/02 - JavaScript/m01/06-Condicionais/script.js
@@ -0,0 +1,19 @@
+
+var hora = 19;
+
+if ( hora < 12 ) {
+    console.log("Bom dia!");
+} else if ( hora < 18 ) {
+    console.log("Boa tarde!");
+} else {
+    console.log("Boa noite!");
+}
+
+
+// var idade = 20;
+
+// if(idade >= 18) {
+//  console.log("Você pode entrar na festa");
+// } else {
+//  console.log("Você é menor de 18 anos, portanto, não pode entrar");
+// }
