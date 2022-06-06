857463c9511a576bc9118a33b49a16266cfbb499
Mayara Silva <devmayara11@gmail.com>
2022-06-06 18:09:21 -0300

    loop for

---
+++ B7Web/02 - JavaScript/m02/09-loop for/script.js
@@ -0,0 +1,22 @@
+// // for loop
+// // for loop array
+
+// let carros = ["Fusca", "Gol", "Palio", "Uno"];
+
+// let html = '<ul>';
+
+// for (let i in carros) {
+//     html += '<li>' + carros[i] + '</li>';
+// }
+
+// html += '</ul>';
+
+// document.getElementById('demo').innerHTML = html;
+
+// let texto = " ";
+
+// for(let i = 1; i <= 5; i++) {
+//   texto = texto + i + "<br>";
+// }
+
+// //document.getElementById('demo').innerHTML = texto;
