c8e20057fcb538e2c75b068b614fd154acd1cf75
Mayara Silva <devmayara11@gmail.com>
2022-06-08 11:20:52 -0300

    selecionando com querySelector

---
+++ B7Web/02 - JavaScript/m03/04-selecionando com queryselector/script.js
@@ -0,0 +1,6 @@
+
+document.querySelector('#algo');    // pegue o elemento pelo id
+document.querySelector('.algo');    // pegue o elemento pelo class
+document.querySelector('button');   // pegue o elemento pelo tag
+document.querySelectorAll('div');   // pegue todos os elementos pelo tag
+document.querySelectorAll('div')[2];    // pegue o elemento pela posição
