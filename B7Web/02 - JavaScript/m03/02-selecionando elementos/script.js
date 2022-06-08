430d2295fd0f4c57167437bef7b8da37c0e8e75f
Mayara Silva <devmayara11@gmail.com>
2022-06-08 11:09:19 -0300

    selecionando elementos

---
+++ B7Web/02 - JavaScript/m03/02-selecionando elementos/script.js
@@ -0,0 +1,19 @@
+document.getElementById('...')
+document.getElementsByClassName('...')
+document.getElementsByTagName('...')
+document.getElementsByName('...')
+
+document.getElementById('exemplo').innerHTML = 'Mayara Gomes';
+
+document.getElementById('exemplo'); // pegue o elemento pelo id
+
+document.getElementsByClassName('lista'); // pegue todos os elementos pela classe
+
+
+
+
+document.getElementsByClassName('lista')[0].innerHTML = 'Fui alterado'; // pegue o elemento pelo indice
+
+
+
+document.getElementsByTagName('button')[0].innerHTML = 'Fui alterado'; // pegue o elemento pelo tag
