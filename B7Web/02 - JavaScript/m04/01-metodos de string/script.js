161d7ecb8482c35b40332c8878bebf3100474005
Mayara Silva <devmayara11@gmail.com>
2022-06-13 10:21:19 -0300

    Métodos de String (1/3)

---
+++ B7Web/02 - JavaScript/m04/01-metodos de string/script.js
@@ -0,0 +1,12 @@
+
+let nome = 'Mayara Silva';
+
+let resultado = '';
+
+if(nome.indexOf('Silva') > -1){
+    resultado = 'Nome contém Silva';
+}else{
+    resultado = 'Nome não contém Silva';
+}
+
+console.log(resultado);
