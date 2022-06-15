370ae444215160ea18191a82b2eb7edf6dae26dc
Mayara Silva <devmayara11@gmail.com>
2022-06-15 09:44:35 -0300

    Métodos de String

--- B7Web/02 - JavaScript/m04/01-metodos de string/script.js
+++ B7Web/02 - JavaScript/m04/01-metodos de string/script.js
@@ -1,12 +1,28 @@
 
-let nome = 'Mayara Silva';
+// let nome = 'Mayara Silva';
 
-let resultado = '';
+// let resultado = nome.slice(4, 10);
 
-if(nome.indexOf('Silva') > -1){
-    resultado = 'Nome contém Silva';
-}else{
-    resultado = 'Nome não contém Silva';
-}
+// if(nome.indexOf('Silva') > -1){
+//     resultado = 'Nome contém Silva';
+// }else{
+//     resultado = 'Nome não contém Silva';
+// }
 
-console.log(resultado);
+// console.log(resultado);
+
+let name = 'Eu gosto de JavaScript';
+
+let result = name.toUpperCase();    // opção para deixar tudo maiusculo
+
+// let result = name.toLowerCase();    // opção para deixar tudo minusculo
+
+// let result = name.concat(' e gosto de NodeJS');   // opção para concatenar
+
+// let result = name.trim();  // opção para remover espaços em branco
+
+// let result = name.charAt(0);  // opção para pegar o primeiro caracter
+
+// let result = name.split(' ');  // opção para separar a string por espaço
+
+console.log("resultado: ", result);
