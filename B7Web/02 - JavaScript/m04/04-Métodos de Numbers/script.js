0447f573e47c22515e4ea6c1bef1fb8b02a3ee6d
Mayara Silva <devmayara11@gmail.com>
2022-06-15 10:13:38 -0300

    metodos de numbers

---
+++ B7Web/02 - JavaScript/m04/04-Métodos de Numbers/script.js
@@ -0,0 +1,11 @@
+
+//let n = 10.9785493475;
+let n1 = "10";
+
+//let result = n.toString();  // muda o tipo de dado de number para string
+
+//let result = n.toFixed(2);  // arredonda o número para 2 casas decimais
+
+let result = parseInt(n1);  // converte o número string para number // Int // Float
+
+console.log(result);
