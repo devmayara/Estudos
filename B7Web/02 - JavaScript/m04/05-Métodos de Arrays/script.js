3b53754a7fc262834eae362623445908873595c7
Mayara Silva <devmayara11@gmail.com>
2022-06-15 11:04:00 -0300

    update

---
+++ B7Web/02 - JavaScript/m04/05-Métodos de Arrays/script.js
@@ -0,0 +1,21 @@
+
+let list = ['ovo', 'banana', 'abacaxi'];
+
+//let result = list.toString(); // converte o array para string
+
+//let result = list.join(' - ');      // converte o array para string, separando os itens por um delimitador
+
+//let result = list.indexOf('banana'); // retorna o índice do item na lista
+
+//list.shift(); // remove o primeiro item da lista
+//list.pop(); // remove o último item da lista
+
+//list.push('uva'); // adiciona um item ao final da lista
+
+//list.unshift('uva'); // adiciona um item no início da lista
+
+list[0] = 'Ovos'; // substitui o primeiro item da lista
+
+let result = list;
+
+console.log(result);
