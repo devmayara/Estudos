c7f6cf95d1d2bea83acaa2ddd96f7689dbcbec9f
Mayara Silva <devmayara11@gmail.com>
2022-06-20 15:43:59 -0300

    update

---
+++ B7Web/02 - JavaScript/m04/23-Key, Values e Entries/script.js
@@ -0,0 +1,8 @@
+
+let lista = ['ovo', 'maça', 'pera', 'laranja'];
+
+console.log(Object.entries(lista));
+
+// keys retorna um array com as chaves do objeto
+// values retorna um array com os valores do objeto
+// entries retorna um array com as chaves e valores do objeto
