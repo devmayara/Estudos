865e08249231e226f7f0556d058145eab80b029c
Mayara Silva <devmayara11@gmail.com>
2022-06-20 10:25:04 -0300

    update

---
+++ B7Web/02 - JavaScript/m04/15-Templete Strings/script.js
@@ -0,0 +1,9 @@
+
+let nome = 'Mayara';
+let idade = 26;
+
+//let frase = 'Ola '+nome+'! Você tem '+idade+' anos.';   //ES5
+
+let frase = `Ola ${nome}! Você tem ${idade} anos.`;   //ES6
+
+console.log(frase);
