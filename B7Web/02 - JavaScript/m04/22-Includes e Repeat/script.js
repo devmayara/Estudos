d48b90656b04ca95bbda1f25abb9a26520dce657
Mayara Silva <devmayara11@gmail.com>
2022-06-20 15:12:10 -0300

    update

---
+++ B7Web/02 - JavaScript/m04/22-Includes e Repeat/script.js
@@ -0,0 +1,9 @@
+
+let lista = ['ovo', 'feijão', 'batata'];
+
+console.log(lista.includes('ovo'));     // includes verifica se existe um elemento na lista
+
+
+
+// repeat repete um elemento quantas vezes for necessária
+console.log('x'.repeat(30));
