1245bb26ceb133b9660ac672b155e50cc34b1ac6
Mayara Silva <devmayara11@gmail.com>
2022-06-20 14:38:53 -0300

    update

---
+++ B7Web/02 - JavaScript/m04/19-Arrow Functions/script.js
@@ -0,0 +1,16 @@
+
+// function somar(x, y) {
+//     return x + y;
+// }
+
+// let somar = function(x, y) {
+//     return x + y;
+// }
+
+let somar = (x, y) => x + y;
+
+// let letrasNoNome = (nome) => nome.length;
+let letrasNoNome = nome => nome.length;
+
+console.log(somar(2, 3));
+console.log(letrasNoNome('João'));
