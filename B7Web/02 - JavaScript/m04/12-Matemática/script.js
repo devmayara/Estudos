27b51a1c7bcee5fa6a0ab20e9e507bb6b637df68
Mayara Silva <devmayara11@gmail.com>
2022-06-20 09:36:35 -0300

    update

---
+++ B7Web/02 - JavaScript/m04/12-Matemática/script.js
@@ -0,0 +1,9 @@
+
+//let novoValor = Math.round(Math.PI);  
+let novoValor = Math.floor(Math.random() * 100);
+    // floor() arredonda o valor para o inteiro anterior mais próximo
+    // ceil() arredonda o valor para o inteiro mais próximo
+    // round() arredonda o valor para o inteiro mais próximo
+    // random() gera um número aleatório entre 0 e 1
+    // abs() retorna o valor absoluto
+console.log(novoValor);
