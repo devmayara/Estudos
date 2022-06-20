f81bd9e092ecdd62c8e47b80ceed37249d6243f6
Mayara Silva <devmayara11@gmail.com>
2022-06-20 09:19:08 -0300

    update

---
+++ B7Web/02 - JavaScript/m04/09-Datas/script.js
@@ -0,0 +1,7 @@
+
+let d = new Date();                 // '2020-01-01 15:42:17'
+
+d.setFullYear( d.getFullYear() + 3 );   // para adicionar 3 anos a data
+
+let novoValor = d;
+console.log(novoValor);
