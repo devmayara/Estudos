be94b693ca4aa23a597592f0a3301012ccd19731
Mayara Silva <devmayara11@gmail.com>
2022-06-21 10:38:32 -0300

    update

---
+++ B7Web/02 - JavaScript/m05/06-Callbacks/script.js
@@ -0,0 +1,12 @@
+
+function alertar() {
+    console.log("Opa, tudo bem?");
+}
+
+let nome = 'Mayara';
+setTimeout(alertar, 2000);
+let sobrenome = 'Silva';
+console.log('Nome Completo: ' + nome + ' ' + sobrenome);
+
+
+// Callback: função que é passada como parâmetro de outra função, você a cria e depois ela é executada. Uma função programada para disparar algum evento ao usuário, como um clique em um botão.
