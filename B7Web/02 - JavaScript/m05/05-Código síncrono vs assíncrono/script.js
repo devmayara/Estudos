0f5bc42ecb3d4d9495a44d72deffb2099f3fdf88
Mayara Silva <devmayara11@gmail.com>
2022-06-21 10:26:57 -0300

    update

---
+++ B7Web/02 - JavaScript/m05/05-Código síncrono vs assíncrono/script.js
@@ -0,0 +1,11 @@
+
+// Codigo sinrono
+let nome = 'João';
+let sobrenome = 'Silva';
+let completo = nome + ' ' + sobrenome;
+
+// Codigo assincrono
+let nome2 = 'João';
+let sobrenome2 = 'Silva';
+let temperatura = Maquininha.pegarTemperatura();  // assicrona
+let completo2 = nome2 + ' ' + sobrenome2;
