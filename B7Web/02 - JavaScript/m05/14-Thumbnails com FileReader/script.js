bbf97cc42c88cfe00a5c3ba61b96e573292d223e
Mayara Silva <devmayara11@gmail.com>
2022-06-23 10:16:08 -0300

    update

---
+++ B7Web/02 - JavaScript/m05/14-Thumbnails com FileReader/script.js
@@ -0,0 +1,17 @@
+
+function mostrar() {
+    let reader = new FileReader();
+    let imagem = document.getElementById('imagem').files[0];
+
+    reader.onloadend = function() {
+        let img = document.createElement('img');
+        img.src = reader.result;
+        img.width = 200;
+
+        document.getElementById('area').appendChild(img);
+    }
+
+    reader.readAsDataURL(imagem);
+}
+
+// por conta do reader a primeira img nao sera substituida pela segunda assim ambas apareceram.
