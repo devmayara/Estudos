c223db820c1129acfe3e9470223aa4066dea835a
Mayara Silva <devmayara11@gmail.com>
2022-07-06 09:28:20 -0300

    Mesclando Variáveis

--- B7Web/05 - PHP/m01/php/index.php
+++ B7Web/05 - PHP/m01/php/index.php
@@ -1,7 +1,19 @@
 <?php
-
+// Variáveis
 $nome = 'Mayara';   // string
 $idade = 26;        // inteiro
 $dormindo = false;  // booleano
 $vazio = null;      // nulo
 $sobrenome = '';    // vazio
+?>
+
+
+<?php
+// Mesclando Variáveis
+$nome = 'Mayara';
+$sobrenome = 'Silva';
+$idade = 26;
+
+$nomeCompleto = $nome . ' ' . $sobrenome . ' tem ' . $idade . ' anos';
+echo $nomeCompleto;
+?>
