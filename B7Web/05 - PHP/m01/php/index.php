d716217b7800590dc21672682f421a20d248d550
Mayara Silva <devmayara11@gmail.com>
2022-07-06 10:15:29 -0300

    Arrays

--- B7Web/05 - PHP/m01/php/index.php
+++ B7Web/05 - PHP/m01/php/index.php
@@ -15,5 +15,28 @@ $sobrenome = 'Silva';
 $idade = 26;
 
 $nomeCompleto = $nome . ' ' . $sobrenome . ' tem ' . $idade . ' anos';
-echo $nomeCompleto;
+// echo $nomeCompleto;
+?>
+
+
+<?php
+// Arrays
+$ingredientes = [
+    'açucar', 
+    'farinha', 
+    'ovo', 
+    'leite',
+    'fermento'
+];
+
+// echo $ingredientes[4];
+
+$numerosDaMegaSena = [10, 14, 27, 34, 8, 43];
+
+// echo "Número 1: " . $numerosDaMegaSena[0] . "<br>";
+// echo "Número 2: " . $numerosDaMegaSena[1] . "<br>";
+// echo "Número 3: " . $numerosDaMegaSena[2] . "<br>";
+// echo "Número 4: " . $numerosDaMegaSena[3] . "<br>";
+// echo "Número 5: " . $numerosDaMegaSena[4] . "<br>";
+// echo "Número 6: " . $numerosDaMegaSena[5] . "<br>";
 ?>
