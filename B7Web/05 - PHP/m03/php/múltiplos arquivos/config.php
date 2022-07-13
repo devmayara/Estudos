0117f92c4b507a1bb0bad8db9a593a4862b8db31
Mayara Silva <devmayara11@gmail.com>
2022-07-13 10:06:07 -0300

    update

---
+++ B7Web/05 - PHP/m03/php/múltiplos arquivos/config.php
@@ -0,0 +1,4 @@
+<?php 
+
+$usuario = 'root';
+$senha = '';
---
+++ B7Web/05 - PHP/m03/php/múltiplos arquivos/index.php
@@ -0,0 +1,13 @@
+<?php
+
+require_once 'cabecalho.php';
+require_once 'config.php';
+
+echo 'E todo o SITE... <br>';
+
+echo 'Nome do Usuário: ' . $usuario . '<br>';
+
+
+// include : Inclui o arquivo, mas não executa o código.
+// require : Inclui o arquivo e executa o código.
+// _once : Inclui o arquivo apenas uma vez.
