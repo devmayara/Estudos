d28e6e8bef8e65469fd5b78290a1ef70c3045dd2
Mayara Silva <devmayara11@gmail.com>
2022-07-14 07:18:41 -0300

    update

--- B7Web/05 - PHP/m04/HTTP Requests/index.php
+++ B7Web/05 - PHP/m04/HTTP Requests/index.php
@@ -6,6 +6,9 @@ require_once 'header.php';
     <label for="nome">Nome:</label>
     <input type="text" name="nome" id="nome" />
     <br>
+    <label for="email">E-mail:</label>
+    <input type="text" name="email" id="email" />
+    <br>
     <label for="idade">Idade:</label>
     <input type="text" name="idade" id="idade" />
     <br><br>
--- B7Web/05 - PHP/m04/HTTP Requests/recebedor.php
+++ B7Web/05 - PHP/m04/HTTP Requests/recebedor.php
@@ -1,10 +1,12 @@
 <?php
 
-$nome = filter_input(INPUT_POST, 'nome');
-$idade = filter_input(INPUT_POST, 'idade');
+$nome = filter_input(INPUT_POST, 'nome', FILTER_SANITIZE_SPECIAL_CHARS);
+$email = filter_input(INPUT_POST, 'email', FILTER_VALIDATE_EMAIL);
+$idade = filter_input(INPUT_POST, 'idade', FILTER_SANITIZE_NUMBER_INT);
 
-if($nome) {
+if($nome && $email && $idade) {
     echo "Nome: ".$nome."<br>";
+    echo "E-mail: ".$email."<br>";
     echo "Idade: ".$idade."<br>";
 } else {
     header('Location: index.php');
