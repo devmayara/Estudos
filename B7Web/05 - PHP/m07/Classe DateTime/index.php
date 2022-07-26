b0ff8a6129ed52fa4ecbb35a79c6360ca00a5396
Mayara Silva <devmayara11@gmail.com>
2022-07-26 16:22:43 -0300

    update

--- B7Web/05 - PHP/m07/Classe DateTime/index.php
+++ B7Web/05 - PHP/m07/Classe DateTime/index.php
@@ -1,8 +1,21 @@
 <?php
 
-$date = new DateTime();
-$date->add( DateInterval::createFromDateString('7 years 2 days 5 minutes') );
-// sub : miminui
-//$date->setTimezone(new DateTimeZone('America/Sao_Paulo'));
+// $date = new DateTime();
+// $date->add( DateInterval::createFromDateString('7 years 2 days 5 minutes 17 seconds') );
 
-echo $date->format('d/m/Y H:i:s');
+// // sub : miminui
+// //$date->setTimezone(new DateTimeZone('America/Sao_Paulo'));
+
+// echo $date->format('d/m/Y H:i:s');
+
+$today = new DateTime();
+$end = new DateTime('2022-12-31');
+
+$diff = $today->diff($end);
+echo $diff->format('%m meses e %d dias para o Fim do Ano!');
+
+$date1 = new DateTime('2020-01-01');
+$date2 = new DateTime('2020-02-15');
+
+$diff = $date1->diff($date2);
+//echo $diff->format('%a');
