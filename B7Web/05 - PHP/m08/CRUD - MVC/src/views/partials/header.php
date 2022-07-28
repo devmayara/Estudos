1459b7ae0e607cd273fd5f35196c7518ae644369
Mayara Silva <devmayara11@gmail.com>
2022-07-28 09:04:14 -0300

    update

---
+++ B7Web/05 - PHP/m08/CRUD - MVC/src/views/partials/header.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="<?= $base; ?>\assets\css\style.css">
+    <title>CRUD em MVC</title>
+</head>
+<body>
+    <header>
+        <h1>Título do Site</h1>
+    </header>
+    <hr>
